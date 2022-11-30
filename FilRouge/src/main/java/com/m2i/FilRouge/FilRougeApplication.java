ba1970c5465fcbe06368df31794bcfd8f1b2d374
Alois Patoor <palois89@posteo.net>
2022-11-30 17:19:10 +0100

    initializing the Spring Boot project

---
+++ FilRouge/src/main/java/com/m2i/FilRouge/FilRougeApplication.java
@@ -0,0 +1,13 @@
+package com.m2i.FilRouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilRougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilRougeApplication.class, args);
+	}
+
+}
---
+++ FilRouge/src/test/java/com/m2i/FilRouge/FilRougeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2i.FilRouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilRougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
